6fe7a6b154d84a498bea75bb44978bdbc0adad29
Nurullah Mencik <nurullahmencik42@gmail.com>
2025-05-31 20:58:35 +0300

    a

--- routes/couponRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCoupon, getAllCoupons } = require('../controllers/couponController');
-
-
-router.post('/', createCoupon);
-router.get('/', getAllCoupons);
-
-module.exports = router;
