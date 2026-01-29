c6ec9d583d482530f7c109237bb17ec07468b33f
nurullahMencik <nurullahmencik42@gmail.com>
2026-01-29 13:52:23 +0300

    aa

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: ['http://localhost:5173', 'http://konyaereglisatis.com', 'https://konyaereglisatis.com',
-     'https://nurullahmencik.com',"https://portfolio-udemy-frontend.vercel.app/"],
+     'https://nurullahmencik.com', "https://portfolio-udemy-frontend.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
